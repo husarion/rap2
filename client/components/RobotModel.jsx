412cfb31d6bba9bf5b1f78b933e4353ca048c5ef
ympek <sz@ympek.net>
2021-04-28 00:23:37 +0200

    changed hover on targets - now it's green ring around the model

--- client/components/RobotModel.jsx
+++ client/components/RobotModel.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useLoader } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Ring } from 'drei';
 import rosbotModel from '../assets/rosbot.gltf';
 
 export default (props) => {
@@ -32,11 +33,21 @@ export default (props) => {
       <mesh material={materials['Color M07']} geometry={nodes.mesh_0.geometry} />
       <mesh material={materials['0136_Charcoal']} geometry={nodes.mesh_0_1.geometry} />
       <mesh material={materials['0135_DarkGray']} geometry={nodes.mesh_0_2.geometry} />
-      {hover || props.noDefaultHover || props.hovered
+      {props.noDefaultHover
         ? <mesh geometry={nodes.mesh_0_3.geometry}><meshBasicMaterial color={0xba0606} /></mesh>
         : <mesh material={materials['Color M05']} geometry={nodes.mesh_0_3.geometry} />}
       <mesh material={materials['0137_Black']} geometry={nodes.mesh_0_4.geometry} />
       <mesh material={materials['Color M09']} geometry={nodes.mesh_0_5.geometry} />
+
+      {(hover || props.hovered) && (
+      <Ring
+        position={[0, 1, 0]}
+        args={[0.16, 0.18, 32]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <meshBasicMaterial attach="material" color={0x00ff00} />
+      </Ring>
+      )}
     </group>
   );
 };
