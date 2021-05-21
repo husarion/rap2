7474dd647fe83d4414c6cc2e62183a651ee12fec
ympek <sz@ympek.net>
2021-05-21 12:16:34 +0200

    rosbot Dockerfile update

--- examples/rosbot/start.sh
+++ examples/rosbot/start.sh
@@ -2,4 +2,4 @@
 sudo service ssh start
 source /opt/ros/noetic/setup.sh
 source /home/husarion/ros_workspace/devel/setup.sh
-roslaunch /home/husarion/ros_workspace/src/gazebo_all.launch
+roslaunch /home/husarion/ros_workspace/src/rosbot_all.launch
